e065c11b56dd72e6874b0b3ae10eb64895f3c39e
icepy <xiangwenwe@foxmail.com>
2016-03-05 14:37:48 +0800

    add dev

--- dist/js/anchor-4a3d4d4520.main.js
+++ dist/js/anchor-4a3d4d4520.main.js
@@ -1,1 +1,9 @@
+/**
+ * @project yinyuetai_pc_live_video
+ * @description 音悦台直播PC版
+ * @version v1.0.0
+ * @time 2016-3-5   14:37:16
+ * @author icepy
+ * @copy http://www.yinyuetai.com
+ */
 webpackJsonp([0],[function(n,e,t){$(function(){var n=t(2),e=(new n,t(14)),o=(new e,t(18));new o;t(22)})},,function(n,e,t){var o=t(3),r=t(8),i=o.extend({el:"#edit_background",rawLoader:function(){var n=t(13);return n},events:{},beforeMount:function(){this.editBgModel=new r},afterMount:function(){},ready:function(){this.editBgModel.execute(function(n){this.$get("items")},function(n){})}});n.exports=i},,,,,,function(n,e,t){var o=t(9),r=o.extend({url:"{{url_prefix}}/mock/index.json",beforeEmit:function(n){}});n.exports=r},,,,,function(n,e){n.exports="<h1>edit background  #000</h1>"},function(n,e,t){var o=t(3),r=(t(15),t(16),o.extend({el:"#set_Anchor_info",rawLoader:function(){var n=t(17);return n},events:{},beforeMount:function(){},afterMount:function(){},ready:function(){}}));n.exports=r},function(n,e,t){var o=t(9),r=o.extend({url:"{{url_prefix}}/mock/index.json",beforeEmit:function(n){}});n.exports=r},function(n,e,t){var o=t(9),r=o.extend({url:"{{url_prefix}}/mock/index.json",beforeEmit:function(n){}});n.exports=r},function(n,e){n.exports="<h1>set anchor info</h1>"},function(n,e,t){var o=t(3),r=(t(19),t(20),o.extend({el:"#anchor_ctrl_chat",rawLoader:function(){var n=t(21);return n},events:{},beforeMount:function(){},afterMount:function(){},ready:function(){}}));n.exports=r},function(n,e,t){var o=t(9),r=o.extend({url:"{{url_prefix}}/mock/index.json",beforeEmit:function(n){}});n.exports=r},function(n,e,t){var o=t(9),r=o.extend({url:"{{url_prefix}}/mock/index.json",beforeEmit:function(n){}});n.exports=r},function(n,e){n.exports="<h1>send message to client</h1>"},function(n,e){}]);
--- dist/js/anchor-dbf73b9f51.main.js
+++
@@ -1,9 +0,0 @@
-/**
- * @project yinyuetai_pc_live_video
- * @description 音悦台直播PC版
- * @version v1.0.0
- * @time 2016-3-5   14:27:4
- * @author icepy
- * @copy http://www.yinyuetai.com
- */
-webpackJsonp([0],[function(n,e,t){$(function(){var n=t(1),e=(new n,t(13)),o=(new e,t(17));new o;t(21)})},function(n,e,t){var o=t(2),r=t(7),i=o.extend({el:"#edit_background",rawLoader:function(){var n=t(12);return n},events:{},beforeMount:function(){this.editBgModel=new r},afterMount:function(){},ready:function(){this.editBgModel.execute(function(n){this.$get("items")},function(n){})}});n.exports=i},,,,,,function(n,e,t){var o=t(8),r=o.extend({url:"{{url_prefix}}/mock/index.json",beforeEmit:function(n){}});n.exports=r},,,,,function(n,e){n.exports="<h1>edit background  #000</h1>"},function(n,e,t){var o=t(2),r=(t(14),t(15),o.extend({el:"#set_Anchor_info",rawLoader:function(){var n=t(16);return n},events:{},beforeMount:function(){},afterMount:function(){},ready:function(){}}));n.exports=r},function(n,e,t){var o=t(8),r=o.extend({url:"{{url_prefix}}/mock/index.json",beforeEmit:function(n){}});n.exports=r},function(n,e,t){var o=t(8),r=o.extend({url:"{{url_prefix}}/mock/index.json",beforeEmit:function(n){}});n.exports=r},function(n,e){n.exports="<h1>set anchor info</h1>"},function(n,e,t){var o=t(2),r=(t(18),t(19),o.extend({el:"#anchor_ctrl_chat",rawLoader:function(){var n=t(20);return n},events:{},beforeMount:function(){},afterMount:function(){},ready:function(){}}));n.exports=r},function(n,e,t){var o=t(8),r=o.extend({url:"{{url_prefix}}/mock/index.json",beforeEmit:function(n){}});n.exports=r},function(n,e,t){var o=t(8),r=o.extend({url:"{{url_prefix}}/mock/index.json",beforeEmit:function(n){}});n.exports=r},function(n,e){n.exports="<h1>send message to client</h1>"},function(n,e){}]);
