b253f292da002a768a5af83482ff38940896fce5
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-03-11 15:11:14 +0800

    创建

--- src/view/anchor-setting/create-live.view.js
+++ src/view/anchor-setting/create-live.view.js
@@ -1,5 +1,5 @@
 /*
-	clientRender:{bool} //默认设置为false，如果为true，内部将不会调用rawLoader方法或者根据templateUrl请求模版
+ clientRender:{bool} //默认设置为false，如果为true，内部将不会调用rawLoader方法或者根据templateUrl请求模版
  */
 
 
@@ -15,27 +15,40 @@ var BaseView = require('BaseView'); //View的基类
 var YYTIMServer = require('../../lib/YYT_IM_Server');
 
 var View = BaseView.extend({
-	el:'#tab-create', //设置View对象作用于的根元素，比如id
-	rawLoader:function(){ //可用此方法返回字符串模版
-		return require('../../template/anchor-setting/create-live.html');
-	},
-	events:{ //监听事件
+  el: '#tab-create', //设置View对象作用于的根元素，比如id
+  rawLoader: function () { //可用此方法返回字符串模版
+    return require('../../template/anchor-setting/create-live.html');
+  },
+  events: { //监听事件
 
-	},
-	//当模板挂载到元素之前
-	beforeMount:function(){
-		YYTIMServer.init({
+  },
+  //当模板挂载到元素之前
+  beforeMount: function () {
+    YYTIMServer.init({
+      listeners: {
+        onMsgNotify: this.onMsgNotify,
+        onGroupInfoChangeNotify: this.onGroupInfoChangeNotify,
+        groupSystemNotifys: this.groupSystemNotifys
+      }
+    });
+  },
+  //当模板挂载到元素之后
+  afterMount: function () {
 
-		});
-	},
-	//当模板挂载到元素之后
-	afterMount:function(){
+  },
+  //当事件监听器，内部实例初始化完成，模板挂载到文档之后
+  ready: function () {
 
-	},
-	//当事件监听器，内部实例初始化完成，模板挂载到文档之后
-	ready:function(){
-		
-	}
+  },
+  onMsgNotify: function (notifyInfo) {
+    console.log('1', notifyInfo);
+  },
+  onGroupInfoChangeNotify: function (notifyInfo) {
+    console.log('2', notifyInfo);
+  },
+  groupSystemNotifys: function (notifyInfo) {
+    console.log('3', notifyInfo);
+  }
 });
 
 module.exports = View;
