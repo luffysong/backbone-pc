fa3efc0f97bfd7ed461a13d7ded28950ddcb367b
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-03-24 13:42:55 +0800

    build

--- dist/js/common-d4e3f3b581.js
+++ dist/js/common-d4e3f3b581.js
@@ -2,7 +2,7 @@
  * @project yinyuetai_pc_live_video
  * @description 音悦台直播PC版
  * @version v1.0.0
- * @time 2016-3-24   13:34:36
+ * @time 2016-3-24   13:42:22
  * @author YYT
  * @copy http://www.yinyuetai.com
  */
