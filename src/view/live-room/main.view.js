aa5ac7b5cfb765a5b408c8f933f8da4ed2e1b414
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-03-30 19:26:38 +0800

    add pages

--- src/view/live-room/main.view.js
+++ src/view/live-room/main.view.js
@@ -1,5 +1,5 @@
 /*
-	clientRender:{bool} //默认设置为false，如果为true，内部将不会调用rawLoader方法或者根据templateUrl请求模版
+ clientRender:{bool} //默认设置为false，如果为true，内部将不会调用rawLoader方法或者根据templateUrl请求模版
  */
 
 
@@ -17,88 +17,44 @@ var uiConfirm = require('ui.Confirm');
 
 
 var View = BaseView.extend({
-	clientRender: false,
-	el: '#anchorContainerBg', //设置View对象作用于的根元素，比如id
-	events:{ //监听事件
+    clientRender: false,
+    el: '#anchorContainerBg', //设置View对象作用于的根元素，比如id
+    events: { //监听事件
 
-	},
-	//当模板挂载到元素之前
-	beforeMount:function(){
-		var url = URL.parse(location.href);
-		this.roomId = url.query['roomId'] || 1;
+    },
+    //当模板挂载到元素之前
+    beforeMount: function () {
+        var url = URL.parse(location.href);
+        this.roomId = url.query['roomId'] || 1;
 
-	},
-	//当模板挂载到元素之后
-	afterMount:function(){
+    },
+    //当模板挂载到元素之后
+    afterMount: function () {
 
-	},
-	//当事件监听器，内部实例初始化完成，模板挂载到文档之后
-	ready:function(){
+    },
+    //当事件监听器，内部实例初始化完成，模板挂载到文档之后
+    ready: function () {
 
-		this.renderPage();
-	},
+        this.renderPage();
+    },
 
-	renderPage: function(){
-		var RoomTitle = require('./room-title.view');
-		new RoomTitle();
+    renderPage: function () {
+        var RoomTitle = require('./room-title.view');
+        new RoomTitle();
 
-		var ChatView = require('./chat.view');
-		new ChatView();
+        var ChatView = require('./chat.view');
+        new ChatView();
+
+        var AnchorCardView = require('./anchor-card.view');
+        new AnchorCardView();
+
+        var PlayedListView = require('./played-list.view');
+        new PlayedListView();
 
 
-		//$()
-
-		this.init();
 
 
-	},
-	init: function(){
-		var jcarousel = $('.jcarousel');
-
-		jcarousel
-			.on('jcarousel:reload jcarousel:create', function () {
-				var carousel = $(this),
-					width = carousel.innerWidth();
-
-				if (width >= 600) {
-					width = width / 3;
-				} else if (width >= 350) {
-					width = width / 2;
-				}
-
-				carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
-			})
-			.jcarousel({
-				wrap: 'circular'
-			});
-
-		$('.jcarousel-control-prev')
-			.jcarouselControl({
-				target: '-=1'
-			});
-
-		$('.jcarousel-control-next')
-			.jcarouselControl({
-				target: '+=1'
-			});
-
-		$('.jcarousel-pagination')
-			.on('jcarouselpagination:active', 'a', function() {
-				$(this).addClass('active');
-			})
-			.on('jcarouselpagination:inactive', 'a', function() {
-				$(this).removeClass('active');
-			})
-			.on('click', function(e) {
-				e.preventDefault();
-			})
-			.jcarouselPagination({
-				perPage: 1,
-				item: function(page) {
-					return '<a href="#' + page + '">' + page + '</a>';
-				}
-			});
-	}
+    }
 
 });
 
---
+++ src/view/live-room/played-list.view.js
@@ -0,0 +1,71 @@
+/*
+ clientRender:{bool} //默认设置为false，如果为true，内部将不会调用rawLoader方法或者根据templateUrl请求模版
+ */
+
+
+/**
+ * @time {时间}
+ * @author {编写者}
+ * @info {实现的功能}
+ */
+
+'use strict';
+
+var BaseView = require('BaseView'); //View的基类
+
+var View = BaseView.extend({
+    el: '#playedListWrap', //设置View对象作用于的根元素，比如id
+    rawLoader: function () { //可用此方法返回字符串模版
+        return require('../../template/live-room/played-list.html');
+    },
+    events: { //监听事件
+
+    },
+    //当模板挂载到元素之前
+    beforeMount: function () {
+
+    },
+    //当模板挂载到元素之后
+    afterMount: function () {
+
+    },
+    //当事件监听器，内部实例初始化完成，模板挂载到文档之后
+    ready: function () {
+
+        this.initCarousel();
+    },
+    initCarousel: function () {
+        var jcarousel = $('.jcarousel');
+
+        jcarousel
+            .on('jcarousel:reload jcarousel:create', function () {
+                var carousel = $(this),
+                    width = carousel.innerWidth();
+
+                if (width >= 600) {
+                    width = width / 3;
+                } else if (width >= 350) {
+                    width = width / 2;
+                }
+
+                carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+            })
+            .jcarousel({
+                wrap: 'circular'
+            });
+
+        $('.jcarousel-control-prev')
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+    }
+
+});
+
+module.exports = View;
