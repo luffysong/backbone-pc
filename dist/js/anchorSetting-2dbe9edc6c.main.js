88da1ac018786a1fbc03cf7d43ce7c21f57cf56f
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-04-05 14:14:54 +0800

    release

--- dist/js/anchorSetting-2dbe9edc6c.main.js
+++ dist/js/anchorSetting-2dbe9edc6c.main.js
@@ -2,7 +2,7 @@
  * @project yinyuetai_pc_live_video
  * @description 音悦台直播PC版
  * @version v1.0.0
- * @time 2016-4-5   13:3:48
+ * @time 2016-4-5   14:14:45
  * @author YYT
  * @copy http://www.yinyuetai.com
  */
--- dist/js/login-58f333f895.main.js
+++
@@ -1,9 +0,0 @@
-/**
- * @project yinyuetai_pc_live_video
- * @description 音悦台直播PC版
- * @version v1.0.0
- * @time 2016-4-5   13:3:48
- * @author YYT
- * @copy http://www.yinyuetai.com
- */
-webpackJsonp([4],[function(n,o,i){$(function(){var n=i(21),o=i(20),e=window.location,t=(n.parse(e.href),o.get("signout"));if(t)return o.remove("signout"),void(e.href="http://login.yinyuetai.com/logout");var r=i(22),c=i(64),a=i(18),h=new c,s=a.sharedInstanceIMModel(),f=r.sharedInstanceUserModel(),g=i(46),d=function(){s.fetchIMUserSig(function(n){n.anchor?window.location.href="/web/anchorsetting.html":window.location.href="/index.html"},function(n){g.showError("获取签名错误，原因：网络或服务器错误")})};f.isLogined()?d():(o.remove("imSig"),h.on("logined",function(){d()}),h.showLoginDialog())})}]);
