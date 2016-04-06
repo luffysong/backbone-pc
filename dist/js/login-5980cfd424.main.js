2fb0d03ff3fd208da195475b92b5f1da0b1bcf5d
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-04-06 14:04:47 +0800

    release

--- dist/js/login-5980cfd424.main.js
+++ dist/js/login-5980cfd424.main.js
@@ -2,7 +2,7 @@
  * @project yinyuetai_pc_live_video
  * @description 音悦台直播PC版
  * @version v1.0.0
- * @time 2016-4-5   14:14:45
+ * @time 2016-4-6   14:4:37
  * @author YYT
  * @copy http://www.yinyuetai.com
  */
