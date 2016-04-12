babe9ed9ce9f47fb0bd1884c67f6f763d7800696
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-04-12 13:07:11 +0800

    beta

--- dist/js/login-292733505d.main.js
+++ dist/js/login-292733505d.main.js
@@ -2,7 +2,7 @@
  * @project yinyuetai_pc_live_video
  * @description 音悦台直播PC版
  * @version v1.0.0
- * @time 2016-4-12   13:4:33
+ * @time 2016-4-12   13:6:39
  * @author YYT
  * @copy http://www.yinyuetai.com
  */
