c931adc90fff7493d57be9f18774f1c60353f642
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-04-13 09:40:09 +0800

    beta

--- dist/js/anchorSetting-9ebdcf0f3c.main.js
+++ dist/js/anchorSetting-9ebdcf0f3c.main.js
@@ -2,7 +2,7 @@
  * @project yinyuetai_pc_live_video
  * @description 音悦台直播PC版
  * @version v1.0.0
- * @time 2016-4-12   16:18:31
+ * @time 2016-4-13   9:37:47
  * @author YYT
  * @copy http://www.yinyuetai.com
  */
