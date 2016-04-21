471bc74557555b2214eb404ff03abe3641aef599
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-04-21 16:40:47 +0800

    login

--- app/src/module/loginBox/index.js
+++ app/src/module/loginBox/index.js
@@ -169,6 +169,7 @@ function loginSubmit(e) {
   e.preventDefault();
   ajaxForm.encrypto(secret.apply(window, _crytoP));
   if (isPassTest()) {
+    console.log(ajaxForm);
     ajaxForm.setIframeState(true);
     this.submit();
   }
