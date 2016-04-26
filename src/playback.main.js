a4b50ddca85bad22a74e42a9af0476150b6fa996
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-04-26 18:30:44 +0800

    play list

--- src/playback.main.js
+++ src/playback.main.js
@@ -10,5 +10,5 @@ $(function(){
     var MainView = require('./view/playback/main.view');
     new MainView();
 
-    require('../style/less/live-room.less');
+    require('../style/less/playbacklist.less');
 });
---
+++ src/playbacklist.main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by AaronYuan on 4/26/16.
+ */
+$(function(){
+  'use strict';
+  var TopbarView = require('./view/topbar/topbar.view');
+  new TopbarView();
+
+  //var MainView = require('./view/playback/main.view');
+  //new MainView();
+
+  require('../style/less/live-room.less');
+});
