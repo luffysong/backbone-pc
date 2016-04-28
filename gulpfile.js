1bee21b5213f97248a97fc00b0a91968c8040047
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-04-28 11:47:57 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -125,10 +125,11 @@ gulp.task('build:move', ['clean'], function () {
   var dontMovePath = '!./';
   var movePath = './';
   return gulp.src([
-      movePath + 'link/base.library.js',
-      movePath + 'link/webim.js',
-      movePath + 'link/json2.js',
-      movePath + 'link/jQuery/jquery.jcarousel.min.js',
+      movePath + 'link/*.js',
+      //movePath + 'link/webim.js',
+      //movePath + 'link/json2.js',
+      //movePath + 'link/jQuery/jquery.jcarousel.min.js',
+      //movePath + 'link/jQuery/jquery.jcarousel.min.js',
       movePath + 'img/**/*.*',
       movePath + 'web/*.*',
       movePath + '*.html',
