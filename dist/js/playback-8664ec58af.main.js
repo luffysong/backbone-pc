d46bf717d2609c84dbd23080e5b6ccd123978cd6
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-05-06 16:18:04 +0800

    beta

--- dist/js/playback-8664ec58af.main.js
+++ dist/js/playback-8664ec58af.main.js
@@ -2,7 +2,7 @@
  * @project yinyuetai_pc_live_video
  * @description 音悦台直播PC版
  * @version v1.0.0
- * @time 2016-5-6   16:9:57
+ * @time 2016-5-6   16:17:47
  * @author YYT
  * @copy http://www.yinyuetai.com
  */
