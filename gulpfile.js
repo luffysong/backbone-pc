62cab3a1ce1615e3f107b18a59f7628b3961bbf4
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-05-09 18:12:02 +0800

    修正gulp reload 直接监视www目录

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,40 @@ var browser = require('browser-sync');
 var browserSync = browser.create();
 
 
-gulp.task('server',function(){
-    browserSync.init({
-        server:'./app',
-        port:4000
+gulp.task('server', ['build'], function () {
+  browserSync.init({
+    server: './app/www',
+    port: 4000
+  });
+  gulp.watch('./app/**/*.*', function (file) {
+    console.log(file.path);
+    browserSync.reload();
+  });
+  gulp.watch('./app/link/**/*.*', function () {
+    gulp.start('build', function () {
+      browserSync.reload();
     });
-    gulp.watch('./app/**/*.*',function(file){
-        console.log(file.path);
-        browserSync.reload();
-    })
+  });
 });
 
 var gutil = require('gulp-util');
-gulp.task('build',function(){
-    return gulp.src([
-        'app/link/*.*',
-        'app/link/**/*.*',
-    ],{base:'app/'})
-        .pipe(gulp.dest('app/www/'))
+gulp.task('build', function () {
+  return gulp.src([
+               'app/flash/*.*',
+               'app/link/*.*',
+               'app/link/**/*.*'
+             ], {base: 'app/'})
+             .pipe(gulp.dest('app/www/'));
 });
 
-gulp.task('webpack',function(callback){
-    var webpack = require('webpack');
-    var productConfig = require('./bin/webpack.product.config.js');
-    webpack(productConfig,function(err,stats){
-        if(err){
-            throw new gutil.PluginError('webpack',err);
-        }
-        gutil.log('[webpack]',stats.toString());
-        callback();
-    });
+gulp.task('webpack', function (callback) {
+  var webpack = require('webpack');
+  var productConfig = require('./bin/webpack.product.config.js');
+  webpack(productConfig, function (err, stats) {
+    if (err) {
+      throw new gutil.PluginError('webpack', err);
+    }
+    gutil.log('[webpack]', stats.toString());
+    callback();
+  });
 });
