c79f0a77faed6b56a4a8fe3744e28b3147c982d0
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-06-24 16:44:19 +0800

    场控新页面

---
+++ app/src/assistant.js
@@ -0,0 +1,17 @@
+/**
+ * Created by AaronYuan on 4/21/16.
+ */
+
+var $ = require('jquery');
+$(function () {
+  var MainView = require('./views/anchor/main.view');
+  var TopBarView = require('TopBarView');
+
+  var a = new MainView({
+    assistant: true
+  });
+  a = new TopBarView();
+  console.log(a);
+  // 载入CSS
+  require('../stylesheets/anchor.scss');
+});
