ff9b6e73483235068053bb206ddc11c02e5ed660
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-06-28 15:57:05 +0800

    发布beta

---
+++ dist/setting.js
@@ -0,0 +1,25 @@
+webpackJsonp([11],{
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	/**
+	 * Created by AaronYuan on 6/6/16.
+	 */
+	var $ = __webpack_require__(1);
+	$(function () {
+	  __webpack_require__(141);
+	});
+
+
+/***/ },
+
+/***/ 141:
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ }
+
+});
+//# sourceMappingURL=setting.js.map
