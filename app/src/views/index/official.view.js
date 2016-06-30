5f7248b7a8b0bcf252e99a95c0069fff3368cfac
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-06-30 19:32:49 +0800

    首页官方数据补全

--- app/src/views/index/official.view.js
+++ app/src/views/index/official.view.js
@@ -5,6 +5,7 @@
 
 var base = require('base-extend-backbone');
 var BaseView = base.View;
+var _ = require('underscore');
 
 var UserModel = require('UserModel');
 var user = UserModel.sharedInstanceUserModel();
@@ -27,7 +28,8 @@ var View = BaseView.extend({
       type: 'YYT'
     };
 
-    this.asideTpl = this.$el.find('#officialAsideTpl').html();
+    this.asideTpl = require('./template/offical-aside-tpl.html');
+    // this.$el.find('#officialAsideTpl').html();
     this.centerTpl = this.$el.find('#officialCenterTpl').html();
 
     this.channelModel = new ChannelModel();
@@ -35,6 +37,19 @@ var View = BaseView.extend({
   afterMount: function () {
     //  获取findDOMNode DOM Node
     this.wrapDOM = this.$el.find('#officialList');
+    // 模板
+    this.emptyItem = {
+      channelId: 0,
+      channelType: 'YYT',
+      channelName: '敬请期待',
+      desc: '敬请期待',
+      posterPic: '',
+      popularity: 0,
+      creator: {
+        largeAvatar: '',
+        nickName: ''
+      }
+    };
   },
   ready: function () {
     //  初始化
@@ -50,15 +65,25 @@ var View = BaseView.extend({
   renderList: function () {
     var self = this;
     var wrap = this.$el.find('#officialList');
+    var len = 0;
+    var list = [];
     wrap.children().remove();
     wrap.html('');
     var promise = this.channelModel.executeJSONP(this.queryParams);
 
     promise.done(function (res) {
       if (res && res.data && res.msg === 'SUCCESS') {
-        self.bindAside(res.data.slice(1, 3));
-        self.bindCenter(res.data[0]);
-        self.bindAside(res.data.slice(3));
+        len = 5 - ~~res.data.length;
+        list = res.data;
+        while (len-- > 0) {
+          list.push(_.extend({}, self.emptyItem));
+        }
+        // self.bindAside(res.data.slice(1, 3));
+        // self.bindCenter(res.data[0]);
+        // self.bindAside(res.data.slice(3));
+        self.bindAside(list.slice(1, 3));
+        self.bindCenter(list[0]);
+        self.bindAside(list.slice(3));
       }
     });
   },
