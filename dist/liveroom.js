3b8b02f4a801bbf17a6564b640454848ae4466fe
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-07-06 10:23:40 +0800

    发布

--- dist/liveroom.js
+++ dist/liveroom.js
@@ -4772,7 +4772,7 @@ webpackJsonp([7],[
 	  this._props = options.props || {};
 	  this.$attrs = {
 	    id: 'YYTFlash' + (uid++), //  配置id
-	    src: this._props.src || origin + '/flash/RTMPInplayer.swf?t=201606029.1', //  引入swf文件
+	    src: this._props.src || origin + '/flash/RTMPInplayer.swf?t=20160706.1', //  引入swf文件
 	    width: this._props.width || 895,
 	    height: this._props.height || 502,
 	    wmode: this._props.wmode || 'transparent', // 控制显示模型
