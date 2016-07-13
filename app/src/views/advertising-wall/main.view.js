6d8169182d34d2246741e90f163cb18e76262abf
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-07-13 09:59:07 +0800

    fix

--- app/src/views/advertising-wall/main.view.js
+++ app/src/views/advertising-wall/main.view.js
@@ -301,7 +301,9 @@ var View = BaseView.extend({
           self.setUserInfo();
           self.newestListDOM.children().remove();
           self.pageParams.newList.hasNext = true;
-          self.renderNewestList();
+          self.renderNewestList({
+            cursor: null
+          });
           // self.renderHotList();
         } else {
           msgBox.showError(res.msg || '发布告白失败,请稍后重试');
