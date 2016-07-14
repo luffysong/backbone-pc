1c8bf3d5d32f62ccf6c78d96c4479b732a8dc702
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-07-14 17:42:03 +0800

    fix index page

--- app/src/views/index/recommended.view.js
+++ app/src/views/index/recommended.view.js
@@ -103,7 +103,7 @@ var View = BaseView.extend({
   // 设置flash
   setFlash: function (video) {
     var videoInfo = video;
-    if (~~video.status === 2) {
+    if (video.status === 'LIVE') {
       // this.FlashApi = FlashApi.sharedInstanceFlashApi({
       this.FlashApi = new FlashApi({
         el: 'topFlash',
@@ -112,6 +112,7 @@ var View = BaseView.extend({
           height: 550
         }
       });
+
       if (this.FlashApi) {
         this.FlashApi.onReady(function () {
           videoInfo.isIndex = true;
@@ -126,12 +127,17 @@ var View = BaseView.extend({
   gotoLiveHome: function (e) {
     var el = $(e.currentTarget);
     var id = el.attr('data-id');
-    var status = el.attr('data-type');
+    var type = el.attr('data-type');
+    var status = el.attr('data-status');
+
     var url = '';
-    switch (status) {
+    switch (type) {
       case 'FANPA_ROOM':
         //  处理直播,注意大小写
         url = '/liveroom.html?roomId=';
+        if (status === 'VOD') {
+          url = '/playback.html?roomId=';
+        }
         break;
       case 'FANPA_CHANNEL':
         // 处理频道
@@ -167,7 +173,8 @@ var View = BaseView.extend({
       // this.elements.btnGoLiveRoom.attr('data-id', video.videoId);
       this.elements.btnGoLiveRoom.attr({
         'data-id': video.videoId,
-        'data-type': video.videoType
+        'data-type': video.videoType,
+        'data-status': video.status
       });
       this.elements.flashWrap.css({
         'background-image': 'url(' + video.posterPic + ')',
